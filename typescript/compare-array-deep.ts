10cafdb062b0d5d2ba4a0ef79a1c47fa38686aef
Filip Strandberg <filip.strandberg@hyph.com>
2024-06-27 15:05:33 +0200

    update compare deep

--- typescript/compare-array-deep.ts
+++ typescript/compare-array-deep.ts
@@ -7,23 +7,23 @@
  * @example
  * const arrA: object[] = [{ name: { first: 'a', last: 'b' } }];
  * const arrB: object[] = [{ name: { first: 'a', last: 'c' } }];
- * deepContentEqual(arrA, arrB); // false
+ * compareArrayDeep(arrA, arrB); // false
  *
  * const arrC: object[] = [{ name: 'yep' }, { name: 'sure' }];
  * const arrD: object[] = [{ name: 'yep' }, { name: 'sure' }];
- * console.log(deepContentEqual(arrC, arrD)); // true
+ * console.log(compareArrayDeep(arrC, arrD)); // true
  *
  * // Arrays that have the same objects in different positions in the array will return true
  * const arrE: object[] = [{ side: 'left' }, { side: 'right' }];
  * const arrF: object[] = [{ side: 'right' }, { side: 'left' }];
- * console.log(deepContentEqual(arrE, arrF)); // true
+ * console.log(compareArrayDeep(arrE, arrF)); // true
  *
  * @returns true if content of arrays are equal, false otherwise
  */
-export function deepContentEqual(arrA: object[], arrB: object[]): boolean {
+export function compareArrayDeep(arrA: object[], arrB: object[]): boolean {
   if (arrA.length !== arrB.length) return false;
 
-  return arrA.every((objA: object) => arrB.find((objB: object) => deepEqualObj(objA, objB)));
+  return arrA.every((objA: object) => arrB.find((objB: object) => compareObjDeep(objA, objB)));
 }
 
 /**
@@ -33,22 +33,24 @@ export function deepContentEqual(arrA: object[], arrB: object[]): boolean {
  * @param objectB Object to compare
  *
  * @example
- * deepEqualObj({ id: 'hi' }, { id: 'hello' }) // false
- * deepEqualObj({ name: 'yes' }, { name: 'yes' }) // true
+ * compareObjDeep({ id: 'hi' }, { id: 'hello' }) // false
+ * compareObjDeep({ name: 'yes' }, { name: 'yes' }) // true
  *
  * @returns true if objects are equal, false otherwise
  */
-export function deepEqualObj(objectA: object, objectB: object): boolean {
+export function compareObjDeep(objectA: object, objectB: object): boolean {
   const keysA: string[] = Object.keys(objectA);
   const keysB: string[] = Object.keys(objectB);
 
   if (keysA.length !== keysB.length) return false;
 
   for (const key of keysA) {
-    const valA: unknown = objectA[key];
-    const valB: unknown = objectB[key];
+    const keyOfObj = key as keyof object;
+
+    const valA: unknown = objectA[keyOfObj];
+    const valB: unknown = objectB[keyOfObj];
     const areObjects: boolean = isObject(valA) && isObject(valB);
-    if ((areObjects && !deepEqualObj(valA as object, valB as object)) || (!areObjects && valA !== valB)) {
+    if ((areObjects && !compareObjDeep(valA as object, valB as object)) || (!areObjects && valA !== valB)) {
       return false;
     }
   }
